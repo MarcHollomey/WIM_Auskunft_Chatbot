a4649db8332327dc7e8b21839ac6c130cfb4c607
Hollomey Marc, MSc <marc.hollomey@pv.at>
2025-04-10 14:48:44 +0200

    test

---
+++ scripts/chatbot2.py
@@ -0,0 +1,1 @@
+2025-04-10 14:23:21.486 [info] > git log --format=%H%n%aN%n%aE%n%at%n%ct%n%P%n%D%n%B -z --shortstat --diff-merges=first-parent -n50 --skip=0 --topo-order --decorate=full --stdin [35ms]
